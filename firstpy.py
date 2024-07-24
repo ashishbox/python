f35cc6b14500405fcf59700adfb99504bf534478
ashishbox <77797913+ashishbox@users.noreply.github.com>
2024-07-23 21:39:34 -0400

    fileI/O

--- firstpy.py
+++ firstpy.py
@@ -11,3 +11,15 @@ my_name[-2]
 
 
 print('Rishita {0} {1}'.format('is', 'good', 'bad', 'komali'))
+
+
+#Dictonry in python
+my_dict = {'name': 'Rishita', 'age': 24, 'city': 'Chennai'}
+print(my_dict['name'])
+
+my_set = set('aaaaaaaabccc')
+print(my_set)
+
+my_set = set(['a', 'b', 'c', 'd'])
+my_set.add('e')
+my_file = open('ashishbox/python/myfile.txt')
